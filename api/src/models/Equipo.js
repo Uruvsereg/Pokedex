0f1d67f831987e54144cc70b27214b718c51a7e1
Uruvsereg <uruvsereg@hotmail.com>
2023-04-12 14:25:07 -0500

    V0.07

---
+++ api/src/models/Equipo.js
@@ -0,0 +1,39 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define(
+        "equipo",
+        {
+           id: {
+              type: DataTypes.UUID,//identificación alfanumérica casi imposible de tener usuarios duplicados
+              allowNull: false,
+              primaryKey: true,
+              defaultValue: DataTypes.UUIDV4//crea automáticamente el ID
+        //    },
+        //    nombre:{
+        //       type: DataTypes.STRING,
+        //       allowNull: false
+        //    },
+        //    apellido:{
+        //       type: DataTypes.STRING,
+        //       allowNull: false
+     
+        //    },
+        //    correo:{
+        //       type: DataTypes.STRING,
+        //       unique: true,
+        //    },
+        //    nickname:{
+        //       type: DataTypes.STRING,
+        //       allowNull: false,
+        //       unique: true
+     
+        //    },
+        //    password:{
+        //       type: DataTypes.STRING,
+        //       allowNull: false
+     
+           }
+        }
+     )
+}
